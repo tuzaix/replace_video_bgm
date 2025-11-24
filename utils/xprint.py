b5d9534400f36489a66e41055a87729f1a8d2a2a
senc <senchen2013@gmail.com>
2025-11-24 18:28:10 +0800

    fg

--- utils/xprint.py
+++ utils/xprint.py
@@ -3,9 +3,8 @@ import os
 # 调试用
 # 打印调试信息
 def xprint(*values: object) -> dict:
-    
     # 在系统上设置 DEBUG=1 来启用调试打印，默认不打印，
-    # 例如：export DEBUG=1 或 set DEBUG=1
+    # 例如：linux: export DEBUG=1 或 win: $env:DEBUG=1
     isDebug = os.getenv("DEBUG", "0") == "1"
     if isDebug:
         print(*values)
